4b4a5316bd80c9ded45baae189df56bb439441c6
danokundaye <dokundaye40@gmail.com>
2023-06-29 21:34:23 +0100

    complete business site page

--- js/page.js
+++ js/page.js
@@ -18,23 +18,29 @@
     // });
 
     $(document).ready(function() {
-        $('#accordion li label').click(function() {
-          var radioButton = $(this).prev('input[type="radio"]');
-          var accordionContent = $(this).siblings('.content');
-          var accordionIcon = $(this).find('.icon');
-      
-          // Toggle the checked state of the radio button
-          radioButton.prop('checked', !radioButton.prop('checked'));
-      
-          // Toggle the content visibility and apply animation
-          accordionContent.slideToggle(200);
-      
-          // Toggle the rotate class on the icon
-          accordionIcon.toggleClass('rotate');
-      
-          // Close other open accordions
-          $('#accordion li .content').not(accordionContent).slideUp(200);
-          $('#accordion li .icon').not(accordionIcon).removeClass('rotate');
-        });
+      $('#accordion li label').click(function() {
+        var radioButton = $(this).prev('input[type="radio"]');
+        var accordionIcon = $(this).find('.icon');
+        var content = $(this).nextAll('.content').first();
+    
+        // Toggle the checked state of the radio button
+        radioButton.prop('checked', !radioButton.prop('checked'));
+    
+        // Toggle the rotate class on the icon
+        accordionIcon.toggleClass('rotate');
+    
+        // Toggle the content visibility and apply animation
+        if (content.css('max-height') === '0px') {
+          content.css('max-height', '400px');
+        } else {
+          content.css('max-height', '0px');
+        }
+    
+        // Close other open accordions
+        $('#accordion li .icon').not(accordionIcon).removeClass('rotate');
+        $('#accordion li .content').not(content).css('max-height', '0px');
       });
-      
+    });
+    
+    
+    
