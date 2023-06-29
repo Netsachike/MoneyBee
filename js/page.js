c322be5b0088af1a97ac9b071489ce5a8a8ff7f3
danokundaye <dokundaye40@gmail.com>
2023-06-29 21:02:55 +0100

    more

--- js/page.js
+++ js/page.js
@@ -1,27 +1,40 @@
-    document.addEventListener('DOMContentLoaded', () => {
-        const slides = document.querySelector('.slides');
-        const radioInputs = document.querySelectorAll('.radio');
+    // document.addEventListener('DOMContentLoaded', () => {
+    //     const slides = document.querySelector('.slides');
+    //     const radioInputs = document.querySelectorAll('.radio');
 
-        slides.addEventListener('scroll', () => {
-            const scrollPos = slides.scrollLeft;
-            const slideWidth = slides.offsetWidth;
-            const activeIndex = Math.round(scrollPos / slideWidth);
-            radioInputs[activeIndex].checked = true;
+    //     slides.addEventListener('scroll', () => {
+    //         const scrollPos = slides.scrollLeft;
+    //         const slideWidth = slides.offsetWidth;
+    //         const activeIndex = Math.round(scrollPos / slideWidth);
+    //         radioInputs[activeIndex].checked = true;
+    //     });
+
+    //     radioInputs.forEach((input) => {
+    //         input.addEventListener('change', () => {
+    //             const slideIndex = Array.from(radioInputs).indexOf(input);
+    //             slides.scroll({ left: slideIndex * slides.offsetWidth, behavior: 'smooth' });
+    //         });
+    //     });
+    // });
+
+    $(document).ready(function() {
+        $('#accordion li label').click(function() {
+          var radioButton = $(this).prev('input[type="radio"]');
+          var accordionContent = $(this).siblings('.content');
+          var accordionIcon = $(this).find('.icon');
+      
+          // Toggle the checked state of the radio button
+          radioButton.prop('checked', !radioButton.prop('checked'));
+      
+          // Toggle the content visibility and apply animation
+          accordionContent.slideToggle(200);
+      
+          // Toggle the rotate class on the icon
+          accordionIcon.toggleClass('rotate');
+      
+          // Close other open accordions
+          $('#accordion li .content').not(accordionContent).slideUp(200);
+          $('#accordion li .icon').not(accordionIcon).removeClass('rotate');
         });
-
-        radioInputs.forEach((input) => {
-            input.addEventListener('change', () => {
-                const slideIndex = Array.from(radioInputs).indexOf(input);
-                slides.scroll({ left: slideIndex * slides.offsetWidth, behavior: 'smooth' });
-            });
-        });
-    });
-
-    //rotate icon
-
-// let rotate = document.querySelectorAll("li")
-
-// for (let i = 0; i < rotate.length; i++) {
-//     if (rotate[i] === )
-    
-// }
+      });
+      
